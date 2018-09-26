69471f238733f58563202905c06c773a6e020aca
Sergey Ukhlivanov <ukhlivanov@gmail.com>
2018-09-25 22:41:49 -0700

    Test

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 "use strict";
 exports.DATABASE_URL =
-  process.env.DATABASE_URL || 'mongodb://localhost/trip';
+  process.env.DATABASE_URL || 'mongodb://tripuser:Paswword1@ds251112.mlab.com:51112/trips';
 exports.TEST_DATABASE_URL =
   process.env.TEST_DATABASE_URL || "mongodb://localhost/test-trip";
 exports.PORT = process.env.PORT || 8080;
