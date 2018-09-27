eef4e98995d12c71f05e3b4800b046da61733e5b
Sergey Ukhlivanov <ukhlivanov@gmail.com>
2018-09-27 14:40:01 -0700

    configured .travis.yml

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,7 @@ module.exports = {
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, './dist'),
-    publicPath: "/assets/"
+    path: path.resolve(__dirname, 'dist')
   },
   devtool: 'inline-source-map',
   module: {
